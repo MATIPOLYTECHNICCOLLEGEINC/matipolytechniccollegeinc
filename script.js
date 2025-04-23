f426f65ea1f4335865c2f740027e254f7b777f62
MATIPOLYTECHNICCOLLEGEINC <mpciicto@gmail.com>
2025-04-23 10:56:59 +0800

    Update script.js
    Signed-off-by: MATIPOLYTECHNICCOLLEGEINC <mpciicto@gmail.com>

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function toggleSidebar() {
   const sidebar = document.getElementById('sidebar');
   if (sidebar.style.right === '0px') {
-    sidebar.style.right = '-100px';
+    sidebar.style.right = '-150px';
   } else {
     sidebar.style.right = '0px';
   }
