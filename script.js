59ac494f2607e42e0c4846783bc71a70cd185b29
MATIPOLYTECHNICCOLLEGEINC <mpciicto@gmail.com>
2025-04-26 13:04:00 +0800

    Update script.js
    Signed-off-by: MATIPOLYTECHNICCOLLEGEINC <mpciicto@gmail.com>

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-//function toggleSidebar() {
- // const sidebar = document.getElementById('sidebar');
- // if (sidebar.style.right === '0px') {
-   // sidebar.style.right = '-150px';
- // } else {
-  //  sidebar.style.right = '0px';
-  }
-}
-
 function toggleSidebar() {
   const sidebar = document.getElementById('sidebar');
-  const isOpen = sidebar.style.right === '0px';
-
-  if (isOpen) {
+  if (sidebar.style.right === '0px') {
     sidebar.style.right = '-150px';
-    document.removeEventListener('click', outsideClickListener);
   } else {
     sidebar.style.right = '0px';
-    setTimeout(() => {
-      document.addEventListener('click', outsideClickListener);
-    }, 0);
-  }
-
-  function outsideClickListener(event) {
-    if (!sidebar.contains(event.target)) {
-      sidebar.style.right = '-150px';
-      document.removeEventListener('click', outsideClickListener);
-    }
   }
 }
